539093c046b0310bf960518effbb5c85590374f8
Karolina Kania <karolina.kania@microsoft.wsei.edu.pl>
2019-11-21 19:37:22 +0100

    zadanie 2 - this

--- 1_Zadania/obowiązkowe/4_Obiekty/js/app.js
+++ 1_Zadania/obowiązkowe/4_Obiekty/js/app.js
@@ -81,3 +81,4 @@ if (spoon.isExist) {
 } else {
     console.log("łyżka nie istnieje");
 }
+
--- 1_Zadania/obowiązkowe/5_this/js/zadanie02.js
+++ 1_Zadania/obowiązkowe/5_this/js/zadanie02.js
@@ -1,11 +1,18 @@
 var calculator = {
-    save: function (newA, newB) {
+    save: function(newA, newB) {
         this.a = newA;
         this.b = newB;
+    },
+    sum: function() {
+        return this.a + this.b;
+    },
+    multiply: function() {
+        return this.a * this.b;
     }
 
 };
 
 calculator.save(2, 3);
+console.log(calculator.a);
 console.log(calculator.sum());
 console.log(calculator.multiply());
