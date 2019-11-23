92a50d009bea258a2e610f620922c80e8380891c
Karolina Kania <karolina.kania@microsoft.wsei.edu.pl>
2019-11-23 18:09:41 +0100

    zadanie 1 + nauka

--- 1_Zadania/obowiązkowe/6_constructor_function/js/app.js
+++ 1_Zadania/obowiązkowe/6_constructor_function/js/app.js
@@ -4,19 +4,20 @@ function Basket() {
     this.products = [];
     this.sum = 0;
     this.addProduct = function(name, price) {
-            this.products.push({
-                name: name,
-                price: price
-            });
-            this.sum += price;
-        },
-        this.showBasket = function() {
-            console.log(this.products);
-            console.log(this.sum);
-        }
+        this.products.push({
+            name: name,
+            price: price
+        });
+        this.sum += price;
+    };
+    this.showBasket = function() {
+        console.log(this.products);
+        console.log(this.sum);
+    }
 }
 
 var aliceBasket = new Basket();
+
 aliceBasket.addProduct("pomidor", 10);
 aliceBasket.addProduct("arbuz", 40);
 aliceBasket.showBasket();
@@ -26,3 +27,52 @@ bruceBasket.addProduct("rice", 10);
 bruceBasket.addProduct("grzyby mun", 50);
 bruceBasket.addProduct("tofu", 20);
 bruceBasket.showBasket();
+
+
+// nauka własna
+
+//pierwszy sposób tworzenia obiektu
+
+var student = {
+    name: "Jan",
+    lastName: "Kowalski",
+    age: 21,
+    id: 11320,
+    printData: function() {
+        console.log(this.name + " " + this.lastName + " wiek " + this.age + " id: " + this.id);
+    }
+};
+student.printData();
+
+// rozszerzanie obiektu
+
+student.city = "Kraków";
+student.showCity = function() {
+    console.log(this.city);
+};
+student.showCity();
+
+// drugi sposób tworzenia obiektu - konstruktor
+
+var University = function(name, studentNumber) {
+    this.name = name;
+    this.studentNumber = studentNumber;
+    this.printData = function() {
+        console.log(this.name + " | " + this.studentNumber);
+    }
+};
+var politechnika = new University("PK", 3200);
+politechnika.printData();
+
+var wsei = new University("WSEI", 9000);
+wsei.printData();
+console.log(wsei);
+University.prototype.addStudent = function(number) {
+    this.studentNumber += number;
+};
+
+politechnika.addStudent(100);
+politechnika.printData();
+wsei.addStudent(500);
+wsei.printData();
+console.log(wsei);
--- 1_Zadania/obowiązkowe/6_constructor_function/js/zadanie01.js
+++ 1_Zadania/obowiązkowe/6_constructor_function/js/zadanie01.js
@@ -1,23 +1,53 @@
-var Robot = function (name) {
+var Robot = function(name) {
     this.name = name;
     this.isFunctional = true;
 }
 
-Robot.prototype.sayHi = function (toWho) {
-    if (isFunctional === true) {
+Robot.prototype.sayHi = function(toWho) {
+    if (this.isFunctional === true) {
         console.log("Robot " + this.name + " greets " + toWho);
-    }
-    else {
-        console.log("Robot " + this.name + "is broken");
+    } else {
+        console.log("Robot " + this.name + " is broken");
     }
 };
 
-Robot.prototype.changeName = function (newname) {
-    console.log("Robot " + this.name + "changes name to " + newname);
-    name = newname;
+Robot.prototype.changeName = function(newname) {
+    console.log("Robot " + this.name + " changes name to " + newname);
+    this.name = newname;
 };
 
-Robot.prototype.fixIt = function () {
+Robot.prototype.fixIt = function() {
     this.isFunctional = true;
-    console.log("Robot " + this.name + "was fixed");
+    console.log("Robot " + this.name + " was fixed");
 };
+
+Robot.prototype.isPanda = false;
+
+Robot.prototype.checkType = function() {
+    if (this.isPanda === true) {
+        console.log("Type is panda!");
+    } else {
+        console.log("Type is not panda!");
+    }
+};
+
+var myRobot = new Robot("cyborg");
+console.log(myRobot.name);
+
+myRobot.sayHi("Karolina");
+myRobot.isFunctional = false;
+myRobot.sayHi("Karolina");
+myRobot.fixIt();
+myRobot.sayHi("Karolina");
+myRobot.changeName("Cyborg 2");
+myRobot.sayHi("Karolina");
+
+var robotMatt = new Robot("Matt");
+robotMatt.sayHi("Karolina");
+robotMatt.checkType();
+
+
+var pandaRobot = new Robot("Pandabot");
+pandaRobot.isPanda = true;
+pandaRobot.sayHi("Karolina");
+pandaRobot.checkType();
